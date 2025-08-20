c5b21ab8323674b45beaf9eb22499885aa1ab6bd
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-08-20 13:32:25 +1200

    Update workflow

---
+++ CorpsAPI/Migrations/20250819040401_AddUserStrikeProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CorpsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStrikeProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AttendanceStrikeCount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfLastStrike",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttendanceStrikeCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfLastStrike",
+                table: "AspNetUsers");
+        }
+    }
+}
