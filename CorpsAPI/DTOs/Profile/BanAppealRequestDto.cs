01726390478034df0e6669aa6d59046b345f67e0
TheRadBazz <125416692+TheRadBazz@users.noreply.github.com>
2025-08-26 16:19:57 +1200

    cleaned banned user dto

--- CorpsAPI/DTOs/Profile/BanAppealRequestDto.cs
+++ CorpsAPI/DTOs/Profile/BanAppealRequestDto.cs
@@ -5,13 +5,15 @@ namespace CorpsAPI.DTOs.Profile
         public string Email { get; set; } = default!;
     }
 
-    public class BannedUserDto
+    public sealed class BannedUserDto
     {
+        public string Id { get; set; } = default!;
         public string Email { get; set; } = default!;
-        public string FirstName { get; set; } = "";
-        public string LastName  { get; set; } = "";
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
         public int AttendanceStrikeCount { get; set; }
         public DateOnly? DateOfLastStrike { get; set; }
         public DateTime? SuspensionUntil { get; set; }
     }
+
 }
