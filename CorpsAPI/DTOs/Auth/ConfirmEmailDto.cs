db32e86a19d5d94a79dfa6637ece8a613a211536
Vivi9tt <tingtingyc99@gmail.com>
2026-05-04 02:07:56 +1200

    Revert "Directing email verification to proper website"
    This reverts commit 91e29b93f21fc7a8f10f05d555462d122bb788e6.

--- CorpsAPI/DTOs/Auth/ConfirmEmailDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace CorpsAPI.DTOs.Auth
-{
-    public class ConfirmEmailDto
-    {
-        public string UserId { get; set; } = default!;
-        public string Token { get; set; } = default!;
-    }
-}
